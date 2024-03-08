51b62c840311be6b020017e834fd57115fe50511
Hory4mE <64050156@KMITL.AC.TH>
2024-03-08 09:48:51 +0700

    Wrapped Up [cut off]

--- Customer/component/backButton2.php
+++ Customer/component/backButton2.php
@@ -23,6 +23,20 @@
     }
 </style>
 <body>
+<?php
+
+
+// เช็คว่ามีการคลิกลิงก์ aBack หรือไม่
+if (isset($_GET['aBack'])) {
+    // Unset session ที่คุณต้องการ
+    unset($_SESSION['guest']);
+    unset($_SESSION['id_username']);
+
+    // Redirect to index.php
+    header("Location: index.php");
+    exit();// สามารถเพิ่มการ redirect ไปที่หน้าหลักหรือหน้าอื่น ๆ ได้ตามต้องการ
+}
+?>
     <a class="aBack" href="index.php">
         <i class="fas fa-arrow-left"></i>
     </a>
