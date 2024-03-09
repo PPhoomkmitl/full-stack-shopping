8e97ab24210383cee59614223ad2f45250799440
PPhoomkmitl <phoomzamak123@gmail.com>
2024-03-09 19:39:58 +0700

    login process

--- admin/loginProcess.php
+++ admin/loginProcess.php
@@ -4,16 +4,34 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        include_once '../../dbConfig.php'; 
+        $cx = mysqli_connect("localhost", "root", "", "shopping");
 
-        if($username == 'admin' && $password == '123456'){
-            $_SESSION['admin'] = true;
-            header("Location: ../admin/dashboard/dashboard.php");
+        $select_user = "SELECT * FROM customer_account WHERE Username = '$username'";
+        $run_qry = mysqli_query($cx, $select_user);
+        if (mysqli_num_rows($run_qry) > 0) {
+            while ($row = mysqli_fetch_assoc($run_qry)) {
+                if (password_verify($password, $row['Password'])) {
+                    if($username == 'admin' && $password == 'Admin123$'){                                              
+                        echo "Password match!";
+                        $_SESSION['admin'] = true;
+
+                        header("Location: ../admin/dashboard/dashboard.php");
+                        exit(); 
+                    }
+                    else {
+                        header("Location: ./login.php");
+                        exit;
+                    }
+                } else {
+                    echo "Password Not match!";
+                }
+            }
         }
         else {
             header("Location: ./login.php");
+            exit;
         }
-        mysqli_close($conn);
+        mysqli_close($cx);
     }
 ?>
 
