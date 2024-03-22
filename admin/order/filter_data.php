6125b0862ebe03c3cb4d6238812049338ab74e98
Hory*- <64050156@KMITL.AC.TH>
2024-03-22 22:03:56 +0700

    Filters : Bugs Fixed

--- admin/order/filter_data.php
+++ admin/order/filter_data.php
@@ -107,22 +107,15 @@ if ($msresults !== null && mysqli_num_rows($msresults) > 0) {
             // Options for shipping_status
             $shipping_statusCompare = ['Pending', 'Inprogress', 'Delivered', 'Canceled'];
             foreach ($shipping_statusCompare as $value) {
-                if ($row['shipping_status'] == 'Canceled') {
+                if ($row['shipping_status'] == 'Canceled' && ($value == 'Pending' || $value == 'Inprogress' || $value == 'Delivered')) {
+                    echo "<option value='$value' style='background-color: #ffff; color: black;' disabled>{$value}</option>";
+                } else if ($row['shipping_status'] == 'Delivered' && ($value == 'Pending' || $value == 'Inprogress' || $value == 'Canceled')) {
+                    echo "<option value='$value' style='background-color: #ffff; color: black;' disabled>{$value}</option>";
+                } else if ($row['shipping_status'] == 'Inprogress' && $value == 'Pending') {
+                    echo "<option value='$value' style='background-color: #ffff; color: black;' disabled>{$value}</option>";
+                } else {
                     $selected = ($value == $row['shipping_status']) ? 'selected' : '';
                     echo "<option value='$value' style='background-color: #ffff; color: black;' $selected>{$value}</option>";
-                } else {
-                    if ($value == 'Canceled') {
-                        echo "<option value='$value' style='background-color: #ffff; color: black;' disabled>{$value}</option>";
-                    } else {
-                        if ($row['shipping_status'] == 'Delivered' && ($value == 'Pending' || $value == 'Inprogress')) {
-                            echo "<option value='$value' style='background-color: #ffff; color: black;' disabled>{$value}</option>";
-                        } else if ($row['shipping_status'] == 'Inprogress' && $value == 'Pending') {
-                            echo "<option value='$value' style='background-color: #ffff; color: black;' disabled>{$value}</option>";
-                        } else {
-                            $selected = ($value == $row['shipping_status']) ? 'selected' : '';
-                            echo "<option value='$value' style='background-color: #ffff; color: black;' $selected>{$value}</option>";
-                        }
-                    }
                 }
             }
             
